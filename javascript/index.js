ee220ca3aa443c36b43ba8de8727a5529813a762
Elizeu-Santos <89655803+Elizeu-Santos@users.noreply.github.com>
2021-10-31 16:45:31 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -15,19 +15,30 @@ const splitsElement = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  printSeconds()
+  printMinutes()
+  printMilliseconds() 
 }
 
 function printMinutes() {
   // ... your code goes here
+  minUni.innerHTML = chronometer.minutes[1];
+  minDec.innerHTML = chronometer.minutes[0];
+
 }
 
 function printSeconds() {
   // ... your code goes here
+  secUni.innerHTML = chronometer.seconds[1];
+  secDec.innerHTML = chronometer.seconds[0];
+
 }
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
+  milUni.innerHTML = chronometer.milliseconds[1];
+  milDec.innerHTML = chronometer.milliseconds[0];
 }
 
 function printSplit() {
@@ -55,11 +66,37 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+btnLeft.addEventListener('click', function () {
+
+  if (btnLeft.innerHTML === "START") {
+      btnLeft.innerHTML = "STOP";
+      btnRight.innerHTML = "SPLIT";
+      btnLeft.setAttribute("class", "btn stop");
+      btnRight.setAttribute("class", "btn split");
+      chronometer.startClick();
+    } else if (btnLeft.innerHTML === "STOP") {
+      btnLeft.innerHTML = "START";
+      btnRight.innerHTML = "RESET";
+      btnLeft.setAttribute("class", "btn start");
+      btnRight.setAttribute("class", "btn reset");
+      chronometer.stopClick();
+    }
+
 });
 
+
 // Reset/Split Button
-btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+btnRight.addEventListener('click', function () {
+
+  const newLi = document.createElement("li")
+  const list = document.querySelector("#splits")
+  const allLi = document.getElementsByTagName("ol")
+  if (btnRight.innerHTML === "SPLIT") {
+    list.appendChild(newLi)
+    newLi.innerHTML = chronometer.minutes + ":" + chronometer.seconds;
+  } else if (btnRight.innerHTML === "RESET") {
+    console.log('TEST');
+    chronometer.resetClick();
+    chronometer.printTime()
+  }
 });
